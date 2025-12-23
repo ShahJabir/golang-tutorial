e66ef918a5d7c1f19cf95c63cc0bab1a6c0c22d2
ShahJabir <shahjabirofficial@gmail.com>
2025-12-23 22:41:17 +0600

    program: simples values

---
+++ 2_simple_values/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(1)
+	fmt.Println(1+2)
+	fmt.Println(2.6+2.5)
+	fmt.Println("Hello, This is a string!")
+	fmt.Println(true)
+	fmt.Println(false)
+}
