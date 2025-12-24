0585c8fafa6267369bb635be91130636586c8e3c
ShahJabir <shahjabirofficial@gmail.com>
2025-12-24 13:16:00 +0600

    program: switch fix: conditions logic

--- 5_conditions/main.go
+++ 5_conditions/main.go
@@ -7,19 +7,21 @@ func main() {
 	fmt.Println("Please enter your score (0-100):")
 	var score int
 	fmt.Scanln(&score)
-	if score >= 80 {
+	if score >= 80 && score <= 100 {
 		fmt.Println("Grade: A+")
-	} else if score >= 70 {
+	} else if score >= 70 && score < 80 {
 		fmt.Println("Grade: A")
-	} else if score >= 60 {
+	} else if score >= 60 && score < 70 {
 		fmt.Println("Grade: A-")
-	} else if score >= 50 {
+	} else if score >= 50 && score < 60 {
 		fmt.Println("Grade: B")
-	} else if score >= 40 {
+	} else if score >= 40 && score < 50 {
 		fmt.Println("Grade: C")
-	} else if score >= 33 {
+	} else if score >= 33 && score < 40 {
 		fmt.Println("Grade: D")
-	} else {
+	} else if score < 33 && score >= 0 {
 		fmt.Println("Grade: F")
+	} else {
+		fmt.Println("Invalid score")
 	}
 }
---
+++ 7_switch/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Student Grade Calculator")
+	fmt.Println("Please enter your score (0-100):")
+	var score int
+	fmt.Scanln(&score)
+	switch {
+	case score < 0 || score > 100:
+		fmt.Println("Invalid score")
+	case score >= 80:
+		fmt.Println("Grade: A+")
+	case score >= 70:
+		fmt.Println("Grade: A")
+	case score >= 60:
+		fmt.Println("Grade: A-")
+	case score >= 50:
+		fmt.Println("Grade: B")
+	case score >= 40:
+		fmt.Println("Grade: C")
+	case score > 33:
+		fmt.Println("Grade: D")
+	default:
+		fmt.Println("Grade: F")
+	}
+	whoamI := func(i interface{}){
+		switch v := i.(type) {
+		case int:
+			fmt.Printf("I am an integer: %d\n", v)
+		case float64:
+			fmt.Printf("I am a float: %f\n", v)
+		case string:
+			fmt.Printf("I am a string: %s\n", v)
+		case bool:
+			fmt.Printf("I am a boolean: %t\n", v)
+		default:
+			fmt.Printf("I am of a different type: %T\n", v)
+		}
+	}
+	whoamI(42)
+	whoamI(3.14)
+	whoamI("Hello, Go!")
+	whoamI(true)
+	whoamI([]int{1, 2, 3})
+}
