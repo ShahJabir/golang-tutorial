68b44d31f45164371eeaf5017d583da4a15d1648
ShahJabir <shahjabirofficial@gmail.com>
2025-12-26 22:24:57 +0600

    fix: fixed goroutines

--- 20_goroutines/main.go
+++ 20_goroutines/main.go
@@ -5,14 +5,14 @@ import (
 	"time"
 )
 
-func goroutineExample() {
-	before_func := time.Now()
-	for i := 0; i < 100; i++ {
+func goroutineExample(i int) {
 		go fmt.Println(i)
-	}
-	fmt.Println("Time taken (Go Routines):", time.Since(before_func))
 }
 
 func main() {
-	goroutineExample()
+	before_func := time.Now()
+	for i:=0; i<=100; i++ {
+		goroutineExample(i)
+	}
+	fmt.Println("Time taken (Go Routines):", time.Since(before_func))
 }
