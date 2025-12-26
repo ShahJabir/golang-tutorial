b1a751258d36c689c7e9d4b28d174bb5e200630c
ShahJabir <shahjabirofficial@gmail.com>
2025-12-27 01:39:33 +0600

    learn: add a example email channel

--- 21_channels/main.go
+++ 21_channels/main.go
@@ -17,9 +17,19 @@ func Add(result chan int, a int, b int) {
 	result <- sum
 }
 
+func emailSender(emailChan <-chan string, done chan<- bool) {
+	for email := range emailChan {
+		fmt.Println("Sent email:", email)
+	}
+	done <- true
+}
+
 func main() {
+	// Create channels
 	numChannels := make(chan int)
 	resChannels := make(chan int)
+	done := make(chan bool)
+	emailChannels := make(chan string, 100) // Buffered channel with capacity 100
 
 	//  Receive input to channels from Add function
 	go Add(resChannels, 5, 7)
@@ -30,13 +40,26 @@ func main() {
 	//  Close resChannels after use
 	close(resChannels)
 
+	//  Sending emails to emailChannels
+	go emailSender(emailChannels, done)
+	before_emailChannels := time.Now()
+	for i := 1; i <= 100; i++ {
+		email := fmt.Sprintf("%d@gmail.com", i)
+		emailChannels <- email
+	}
+	fmt.Println("All emails sent to channel")
+	//  Close emailChannels after sending all emails
+	close(emailChannels)
+	<-done
+	fmt.Println("Time taken for email loop:", time.Since(before_emailChannels).Milliseconds(), "ms")
+
 	//  Sending random numbers to numChannels
 	before_numChannels := time.Now()
 	go processNum(numChannels)
-	for i := 0; i < 100; i++ {
+	for i := 1; i <= 100; i++ {
 		numChannels <- rand.Intn(1000)
 	}
-	println("Time taken for random loop:", time.Since(before_numChannels).Nanoseconds(), "ns")
+	fmt.Println("Time taken for random loop:", time.Since(before_numChannels).Microseconds(), "ms")
 	//  Close numChannels after sending all numbers
 	close(numChannels)
 }
