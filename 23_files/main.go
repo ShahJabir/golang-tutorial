b0841e47a5d6c75baff883b8fd322726e8150446
ShahJabir <shahjabirofficial@gmail.com>
2025-12-27 13:14:43 +0600

    learn: read and create files learn: read directory

--- 23_files/main.go
+++ 23_files/main.go
@@ -5,19 +5,79 @@ import (
 	"os"
 )
 
-func main() {
-	f, err := os.Open("./example.txt")
+func fileInfo(filePath string) os.FileInfo {
+	f, err := os.Open(filePath)
 	if err != nil {
 		panic(err)
 	}
+	defer f.Close()
 	fileInfo, err := f.Stat()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("File Info:", fileInfo)
-	fmt.Println("File Name:", fileInfo.Name())
-	fmt.Println("File Size:", fileInfo.Size())
-	fmt.Println("Is Directory:", fileInfo.IsDir())
-	fmt.Println("File Permission:", fileInfo.Mode())
+	return fileInfo
+}
+
+func readDir(dirPath string) []os.FileInfo {
+	f, err := os.Open(dirPath)
+	if err != nil {
+		panic(err)
+	}
 	defer f.Close()
+	fileInfos, err := f.Readdir(-1)
+	if err != nil {
+		panic(err)
+	}
+	return fileInfos
+}
+
+func readFile(filePath string) []byte {
+	f, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	fileInfo := fileInfo(filePath)
+	data := make([]byte, fileInfo.Size())
+	_, err = f.Read(data)
+	if err != nil {
+		panic(err)
+	}
+	return data
+}
+
+func createFile(filePath string, data []byte) {
+	f, err := os.Create(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	_, err = f.Write(data)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	// Get file information
+	info := fileInfo("./example.txt")
+	fmt.Println("File Name:", info.Name())
+	fmt.Println("File Size:", info.Size())
+	fmt.Println("Is Directory:", info.IsDir())
+	fmt.Println("Last Modified:", info.ModTime())
+
+	// Read file content
+	data := readFile("./example.txt")
+	fmt.Println("File Content:", string(data))
+
+	// Read directory contents
+	fileInfos := readDir(".")
+	fmt.Println("Directory Contents:")
+	for _, fileInfo := range fileInfos {
+		fmt.Println(fileInfo.Name())
+	}
+
+	// Create a new file
+	createFile("./newfile.txt", []byte("Hello, World!"))
+	fmt.Println("New file created: newfile.txt")
 }
